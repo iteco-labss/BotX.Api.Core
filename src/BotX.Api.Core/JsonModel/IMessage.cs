b26193d8ce1cd9c99afa9da47c4df76bc621fbdf
gudoff <av.gudov@gmail.com>
2020-01-24 18:24:51 +0300

    Update IMessage.cs
    убрал коммент

--- src/BotX.Api.Core/JsonModel/IMessage.cs
+++ src/BotX.Api.Core/JsonModel/IMessage.cs
@@ -4,8 +4,6 @@ using System.Text;
 
 namespace BotX.Api.JsonModel
 {
-    // TODO обсудить с андреем зачем нам IMessage
-    // сейчас BotId нужен только для нотификации
     internal interface IMessage
     {
         public Guid BotId { get; set; }
