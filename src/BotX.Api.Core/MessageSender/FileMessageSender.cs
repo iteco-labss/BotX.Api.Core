776cf6b2ef926ad4eaeb1ec2004d992e0bbef717
Andrey Gudov <av.gudov@gmail.com>
2020-04-14 19:36:28 +0300

    fix(messages): добавлено ограничение на размер отправляемого сообщения с файлом

--- src/BotX.Api.Core/MessageSender/FileMessageSender.cs
+++ src/BotX.Api.Core/MessageSender/FileMessageSender.cs
@@ -13,7 +13,8 @@ namespace BotX.Api
 	internal partial class BotMessageSender : IBotMessageSender
 	{
 		public async Task SendFileAsync(Guid syncId, string fileName, byte[] data)
-		{
+        {
+            CheckFileSize(data);
 			await httpClient.SendFileAsync(
 				syncId: syncId,
 				botId: ExpressBotService.Configuration.BotId,
@@ -23,7 +24,8 @@ namespace BotX.Api
 		}
 
 		public async Task SendFileAsync(UserMessage requestMessage, string fileName, byte[] data)
-		{
+        {
+            CheckFileSize(data);
 			await SendFileAsync(
 				syncId: requestMessage.SyncId,
 				fileName: fileName,
@@ -32,12 +34,14 @@ namespace BotX.Api
 		}
 
 		public async Task SendFileAsync(Guid chatId, Guid huid, string fileName, byte[] data)
-		{
+        {
+            CheckFileSize(data);
 			await SendFileAsync(chatId, huid, null, fileName, data);
 		}
 
 		public async Task SendFileAsync(Guid chatId, Guid huid, string caption, string fileName, byte[] data)
 		{
+			CheckFileSize(data);
 			var notification = new NotificationMessage
 			{
 				Recipients = new[] { huid },
@@ -51,6 +55,13 @@ namespace BotX.Api
 			};
 			await httpClient.SendNotificationAsync(notification);
 		}
+
+        private void CheckFileSize(byte[] fileData)
+        {
+            const int limitInMb = 100;
+			if (fileData.Length > limitInMb * 1024 * 1024)
+                throw new Exception($"The file size is too large. The maximum file size is limited to {limitInMb} MB");
+        }
 
 		private string GetMimeType(string fileName)
 		{
